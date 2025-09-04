9f0fd34bec2b9996647e327239bb88f09cc2034c
JulyMorga <julianmorganella@hotmail.com>
2025-09-03 21:32:09 -0300

    Agrego la clase Asiento.

---
+++ src/com/morga/saladecine/Asiento.java
@@ -0,0 +1,30 @@
+package com.morga.saladecine;
+
+public class Asiento {
+	private String nombreComprador;
+	private boolean ocupado;
+		
+	public Asiento(String nombreComprador, boolean estado) {
+		this.nombreComprador = nombreComprador;
+		this.ocupado = estado;
+	}
+	
+	public String getNombreComprador() {
+		return nombreComprador;
+	}
+	
+	public void ocupar(String nombreComprador) {
+		this.nombreComprador = nombreComprador;
+		this.ocupado = true;
+	}
+	
+	public void liberar() {
+		this.nombreComprador = "";
+		this.ocupado = false;
+	}
+	
+	public boolean estaOcupado() {
+		return ocupado;
+	}	
+	
+}
